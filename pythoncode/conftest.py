19c93d3b1da56bf5aff829d84c58d8c41c1a3151
qingyi <1259495681@qq.com>
2020-06-16 17:59:57 +0800

    添加pytest课后作业三

--- pythoncode/conftest.py
+++ pythoncode/conftest.py
@@ -1,11 +1,38 @@
 import pytest
+#
+# from pythoncode.cal import calc
+#
+#
+# @pytest.fixture(scope='function')
+# def start():
+#     calc1 = calc()
+#     print("开始计算")
+#     yield calc1
+#    print("计算结束")
+# --------------------------------------------------------------------------------------------------------------\
+import yaml
 
-from pythoncode.cal import calc
+
+def pytest_addoption(parser):
+    mygroup = parser.getgroup("myconfig")
+    mygroup.addoption("--env",
+                      default='test',
+                      dest='env',
+                      help='run env'
+
+                      )
 
 
-@pytest.fixture(scope='function')
-def start():
-    calc1 = calc()
-    print("开始计算")
-    yield calc1
-    print("计算结束")
+@pytest.fixture(scope='session')
+def cmdoption(request):
+    myenv = request.config.getoption("--env", default='test')
+    if myenv == 'test':
+        with open("datas/test/test.yml") as f:
+            datas1 = yaml.safe_load(f)
+    elif myenv == 'dev':
+        with open("datas/dev/dev.yml") as f:
+            datas1 = yaml.safe_load(f)
+    elif myenv == 'st':
+        with open("datas/st/st.yml") as f:
+            datas1 = yaml.safe_load(f)
+    return datas1
